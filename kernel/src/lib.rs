d5e1c25607c6e8016686f9a27d38b8e1e65be626
Victor Koenders <gpg@trangar.com>
2022-01-18 15:54:17 +0100

    Merged projects into workspace

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,8 +1,5 @@
 #![feature(format_args_nl, panic_info_message)]
-#![no_main]
 #![no_std]
-
-use core::panic::PanicInfo;
 
 #[cfg(target_arch = "aarch64")]
 #[path = "arch/aarch64/boot.rs"]
@@ -15,6 +12,10 @@ pub fn kernel_main() -> ! {
     panic!("Stopping here");
 }
 
+#[cfg(target = "aarch64-unknown-none-softfloat")]
+use core::panic::PanicInfo;
+
+#[cfg(target = "aarch64-unknown-none-softfloat")]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     if let Some(args) = info.message() {
