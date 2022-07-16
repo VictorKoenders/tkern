b51a8db8cf336d7d12dca745758d50be86c70a3c
Victor Koenders <gpg@trangar.com>
2022-07-16 15:50:54 +0200

    Added rpi3 AUX_MU_IER_REG, removed expanded.rs

--- sys/derive/src/peripheral/parse.rs
+++ sys/derive/src/peripheral/parse.rs
@@ -186,9 +186,18 @@ pub fn get_struct_fields(input: &DeriveInput) -> Result<Vec<Field>, (String, Spa
             "u8" => {
                 let reset =
                     if let Some(reset) = reset {
-                        Some(reset.to_string().parse().map_err(|_| {
+                        let str = reset.to_string();
+                        let reset = if let Some(base_2) = str.strip_prefix("0b") {
+                            u8::from_str_radix(base_2, 2)
+                        } else if let Some(base_16) = str.strip_prefix("0x") {
+                            u8::from_str_radix(base_16, 16)
+                        } else {
+                            str.parse()
+                        }.map_err(|_| { 
                             ("Invalid reset value for u8".to_string(), reset.span())
-                        })?)
+                        })?;
+
+                        Some(reset)
                     } else {
                         None
                     };
--- sys/rpi3/src/pac/mod.rs
+++ sys/rpi3/src/pac/mod.rs
@@ -16,13 +16,13 @@ pub mod AUX {
     #[derive(sys_derive::Peripheral)]
     #[peripheral(address = 0x21_5000)]
     pub struct IRQ {
-        #[field(bit = 2, readable, reset = false)]
+        #[field(bit = 2, readable)]
         /// If set the SPI 2 module has an interrupt pending.
         pub SPI_2_IRQ: bool,
-        #[field(bit = 1, readable, reset = false)]
+        #[field(bit = 1, readable)]
         /// If set the SPI 1 module has an interrupt pending.
         pub SPI_1_IRQ: bool,
-        #[field(bit = 0, readable, reset = false)]
+        #[field(bit = 0, readable)]
         /// If set the mini UART has an interrupt pending.
         pub MINI_UART_IRQ: bool,
     }
@@ -32,14 +32,14 @@ pub mod AUX {
     #[peripheral(address = 0x21_5004)]
     pub struct ENB {
         /// If set the SPI 2 module is enabled.  If clear the SPI 2 module is disabled. That also disables any SPI 2 module register access
-        #[field(bit = 2, readable, writable, reset = 0)]
+        #[field(bit = 2, readable, writable)]
         pub SPI2_enable: bool,
         /// If set the SPI 1 module is enabled.  If clear the SPI 1 module is disabled. That also disables any SPI 1 module register access
-        #[field(bit = 1, readable, writable, reset = 0)]
+        #[field(bit = 1, readable, writable)]
         pub SPI1_enable: bool,
         /// If set the mini UART is enabled. The UART will immediately start receiving data, especially if the UART1_RX line is low.
         /// If clear the mini UART is disabled. That also disables any mini UART register acces
-        #[field(bit = 0, readable, writable, reset = 0)]
+        #[field(bit = 0, readable, writable)]
         pub Mini_UART_enable: bool,
     }
 
@@ -52,16 +52,42 @@ pub mod AUX {
     pub struct MU_IO_REG {
         /// Access to the LS 8 bits of the 16-bit baudrate register.
         /// (Only If bit 7 of the line control register (DLAB bit) is set)
-        #[field(bits = 7:0, readable, writable, reset = 0)]
+        #[field(bits = 7:0, readable, writable)]
         pub LS_8_bit_baudrate: u8,
         /// Data written is put in the transmit FIFO (Provided it is not full)
         // (Only If bit 7 of the line control register (DLAB bit) is clear)
-        #[field(bits = 7:0, writable, reset = 0)]
+        #[field(bits = 7:0, writable)]
         pub transmit_data_write: u8,
         /// Data read is taken from the receive FIFO (Provided it is not empty)
         /// (Only If bit 7 of the line control register (DLAB bit) is clear)
-        #[field(bits = 7:0, readable, reset = 0)]
+        #[field(bits = 7:0, readable)]
         pub receive_data_read: u8,
+    }
+
+    /// The `AUX_MU_IER_REG` register is primary used to enable interrupts
+    /// If the DLAB bit in the line control register is set this register gives access to the MS 8 bits
+    /// of the baud rate. (Note: there is easier access to the baud rate register)
+    #[derive(sys_derive::Peripheral)]
+    #[peripheral(address = 0x21_5044)]
+    pub struct MU_IER_REG {
+        /// Access to the MS 8 bits of the 16-bit baudrate register.
+        /// (Only If bit 7 of the line control register (DLAB bit) is set)
+        #[field(bits = 7:0, readable, writable)]
+        pub MS_8_bits_baudrate: u8,
+
+        /// Must be set to 0b11 to receive interrupts
+        #[field(bits = 3:2, writable)]
+        pub enable_interrupts: u8,
+
+        /// If this bit is set the interrupt line is asserted whenever the transmit FIFO is empty.
+        /// If this bit is clear no transmit interrupts are generated.
+        #[field(bit = 1, readable, writable)]
+        pub Enable_transmit_interrupts: bool,
+
+        /// If this bit is set the interrupt line is asserted whenever the receive FIFO holds at least 1 byte
+        /// If this bit is clear no receive interrupts are generated.
+        #[field(bit = 1, readable, writable)]
+        pub Enable_receive_interrupts: bool
     }
 }
 
