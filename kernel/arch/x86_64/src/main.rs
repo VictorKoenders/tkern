826d3fef4f1ee9997c90cf4c4fc512fe2f181baa
Victor Koenders <victor.koenders@qrtech.se>
2023-01-16 20:09:11 +0100

    Added GraphicsOutput framebuffer rendering

--- kernel/arch/x86_64/src/main.rs
+++ kernel/arch/x86_64/src/main.rs
@@ -10,10 +10,9 @@ use core::panic::PanicInfo;
 use uefi::{
     prelude::entry,
     proto::console::{
-        gop,
+        gop::{self, FrameBuffer, ModeInfo},
         text::{Key, ScanCode},
     },
-    table::boot::SearchType,
 };
 use uefi_services::println;
 
@@ -43,32 +42,54 @@ fn efi_main(
 }
 
 fn try_render_square(table: &mut uefi::table::SystemTable<uefi::table::Boot>) {
-    let handles = table
+    let handle = table
         .boot_services()
-        .locate_handle_buffer(SearchType::from_proto::<gop::GraphicsOutput>())
+        .get_handle_for_protocol::<gop::GraphicsOutput>()
         .unwrap();
-    println!("Found {} handles", handles.handles().len());
-    for handle in handles.handles().iter().skip(1) {
-        println!("  {handle:?}");
-        match table
-            .boot_services()
-            .open_protocol_exclusive::<gop::GraphicsOutput>(*handle)
-        {
-            Ok(gop) => {
-                let modes = gop.modes();
-                println!("    {} modes", modes.len());
-                for mode in modes {
-                    let (width, height) = mode.info().resolution();
-                    let pixel_format = mode.info().pixel_format();
-                    let stride = mode.info().stride();
-                    println!(
-                        "        resolution: {width}x{height}, pixel format: {pixel_format:?}, stride: {stride}"
-                    );
+    let mut gop = table
+        .boot_services()
+        .open_protocol_exclusive::<gop::GraphicsOutput>(handle)
+        .unwrap();
+    let mode_info = gop.current_mode_info();
+    let mut fb = gop.frame_buffer();
+
+    let pixel = Color::WHITE;
+    for offset_x in 0..8 {
+        for offset_y in 0..4 {
+            for x in 0..50 {
+                for y in 0..50 {
+                    let x = x + (offset_x * 50);
+                    let y = y + (offset_y * 100) + if offset_x % 2 == 1 { 50 } else { 0 };
+                    pixel.write(&mut fb, (x, y), &mode_info);
                 }
             }
-            Err(e) => {
-                println!("    Could not load: {e:?}");
-            }
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl Color {
+    pub const WHITE: Color = Color::new(255, 255, 255);
+    pub const fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+    pub fn write(self, target: &mut FrameBuffer, (x, y): (u16, u16), format: &ModeInfo) {
+        let offset = (x as usize + y as usize * format.stride()) * 4;
+        match format.pixel_format() {
+            gop::PixelFormat::Rgb => unsafe {
+                target.write_value(offset, [self.r, self.g, self.b]);
+            },
+            gop::PixelFormat::Bgr => unsafe {
+                target.write_value(offset, [self.b, self.g, self.r]);
+            },
+            gop::PixelFormat::Bitmask => unimplemented!(),
+            gop::PixelFormat::BltOnly => unimplemented!(),
         }
     }
 }
